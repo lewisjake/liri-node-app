c85f24a58ed48a5f62e0af724ceae153d7359c49
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-10-01 18:38:16 -0700

    created switch statement to match the user input with the function being called.

--- liri.js
+++ liri.js
@@ -1,9 +1,36 @@
 // required libraries
 require("dotenv").config();
 var fs = require("fs");
-var keys = require("./keys.js");
+const keys = require("./keys.js");
 var moment = require('moment');
 var axios = require("axios");
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
+const Spotify = require('node-spotify-api');
+let spotify = new Spotify(keys.spotify);
+let command = process.argv[2];
+let searchTerm = process.argv[3];
+var spotifyTest = process.env.SPOTIFY_ID
+ 
+fs.appendFile('log.txt', command + ",", function(err) {
+    if (err) throw err;
+});
+
+// swtich command to gather the input from the user. if user entry is error, display message
+switch (command) {
+    case "concert-this": // BandsInTown
+    searchBIT(searchTerm);
+    break;
+    
+    case "spotify-this-song": // Spotify
+    searchThisSong(searchTerm);
+    break;
+    
+    case "movie-this": // OMDB
+    movieThis(searchTerm);
+    break;
+
+    case "do-what-it-says": // Random.txt
+    doRandom();
+    break;
+}
+
 
